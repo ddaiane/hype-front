064d3bf4272f21d72ff595d96ae9b03ecff055ea
Daiane <daianemarcon@gmail.com>
2021-10-05 04:10:10 -0300

    page cadastra predio pronta

--- src/pages/CadastraPredio/index.js
+++ src/pages/CadastraPredio/index.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Header from '../../components/Header';
 import SlidesImagens from '../../components/Carousel';
 import FormNovoPredio from '../../components/FormNovoPredio';
-//import './styles.css';
+import './styles.css';
 
 
 
